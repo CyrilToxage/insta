58737ac71eb44b87dfe446b254b8f23c3021b732
CyrilToxage <mcyril2106@gmail.com>
2024-10-27 23:44:04 +0100

    0.0.3 -- fin de creation de migration

--- database/migrations/2024_10_27_223838_create_followers_table.php
+++ database/migrations/2024_10_27_223838_create_followers_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('followed_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['follower_id', 'followed_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('followers');
